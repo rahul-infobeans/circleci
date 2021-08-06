7c661dfc4e4b6fed06a703c0545c7355d9a4cefb
Yash Chopra <yash.chopra@infobeans.com>
2021-08-06 17:20:52 +0530

    fix config file

--- private/bin/run_pr_checks.sh
+++ private/bin/run_pr_checks.sh
@@ -9,6 +9,12 @@ echo "Starting WP-Cypress"
 yarn wp-cypress start
 
 #tail -100 ./node_modules/@bigbite/wp-cypress/debug.log
+## background your server
+yarn start &
+
+## poll the server over and over again
+## until it's been booted
+wait-on http://localhost:8080
 
 echo "Running WP-Cypress"
 yarn cypress run --browser chrome
