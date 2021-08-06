a19bffc56c01f286daaa80d3c6f6125e62d6308f
Yash Chopra <yash.chopra@infobeans.com>
2021-08-06 18:15:16 +0530

    fix config file

--- private/bin/run_pr_checks.sh
+++ private/bin/run_pr_checks.sh
@@ -12,11 +12,11 @@ yarn wp-cypress start
 ## background your server
 
 npm install wait-on 
-yarn start
+#yarn start
 
 ## poll the server over and over again
 ## until it's been booted
-#yarn wait-on http://localhost:8080
+yarn wait-on http://localhost:8080
 
 echo "Running WP-Cypress"
 sudo yarn cypress run --browser chrome
